9c8902ac2f6273986ebfb0940bcd9def14668696
i3 <i3@debian-BULLSEYE-live-builder-AMD64>
2021-12-22 19:23:06 +0100

    feat(vue): basic vuecli template

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container border bg-light">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Thi is a direct messahge from the App.vue file."/>
+    <BasicInfomration/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BasicInfomration from './components/BasicInfomration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BasicInfomration
   }
 }
 </script>
 
 <style>
-#app {
+#appl {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/BasicInfomration.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container border">
+    <ul>
+      <li v-for="(value, key) in informations" :key="key">
+        {{ key }} : {{ value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      informations: {
+        'Teljes név': 'Kovács Norbert',
+        'Születési dátum': '1995.02.24',
+        'Tanulmányok': 'Programtervező Informatika',
+        'Telefon': '+36 70 517 09 49',
+        'E-mail': 'mfw.kovacs.norbert@gmail.com',
+        'Lakhely': 'Eger'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
